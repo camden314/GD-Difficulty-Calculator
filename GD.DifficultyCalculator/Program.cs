93660d1d86ed25f4f962f16135db69542dca289a
Noah <altenyt@gmail.com>
2020-02-09 21:50:36 -0500

    Initial project

---
+++ GD.DifficultyCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GD.Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
